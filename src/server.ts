2211de812e64dd0492dfbc83ca73378edd550795
vibraniumSwaleh <hus.swaleh@yahoo.com>
2022-08-27 00:31:23 +0100

    added error 422 and sample working link

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,11 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   //! END @TODO1
 
+  /* https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg cannot be used since this URL will not work correctly with CORS. It does not have the access-control-allow-origin header set to *. Without this header, requests from other domains cannot be made to it via a users browser. */
+
+  /* sample link that works:  
+  http://localhost:8082/filteredimage?image_url=https%3A%2F%2Fcdn.motor1.com%2Fimages%2Fmgl%2Fmrz1e%2Fs1%2Fcoolest-cars-feature.jpg&imgrefurl=https%3A%2F%2Fwww.motor1.com%2Ffeatures%2F447037%2Fcoolest-cars%2F&tbnid=z4t7U0uZlwRpRM&vet=12ahUKEwi73Nv5yuX5AhXvSkEAHbdtBJoQMygNegUIARCSAw..i&docid=LBt-IiFPpVXXIM&w=1920&h=1080&q=car&ved=2ahUKEwi73Nv5yuX5AhXvSkEAHbdtBJoQMygNegUIARCSAw */
+
   app.get(
     "/filteredimage",
     async (req: express.Request, res: express.Response) => {
@@ -39,16 +44,18 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
       if (!image_url) {
         res.status(400).send("Image url required!!!");
       } else {
-      //    2. call filterImageFromURL(image_url) to filter the image
-        await filterImageFromURL(image_url).then(function (
-          image_filtered_path
-        ) {
-      //    3. send the resulting file in the response
-          res.sendFile(image_filtered_path, () => {
-      //    4. deletes any files on the server on finish of the response
-            deleteLocalFiles([image_filtered_path]);
+        //    2. call filterImageFromURL(image_url) to filter the image
+        await filterImageFromURL(image_url)
+          .then(function (image_filtered_path) {
+            //    3. send the resulting file in the response
+            res.sendFile(image_filtered_path, () => {
+              //    4. deletes any files on the server on finish of the response
+              deleteLocalFiles([image_filtered_path]);
+            });
+          })
+          .catch(function (err) {
+            res.status(422).send("Image was not filtered!!!");
           });
-        });
       }
     }
   );
