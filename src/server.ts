3d2c20e05d1a3c75154381fd9b4d299a16f37cbe
vibraniumSwaleh <hus.swaleh@yahoo.com>
2022-08-28 19:10:06 +0100

    node version v14 used, deployment error fixed, app working!

--- src/server.ts
+++ src/server.ts
@@ -36,14 +36,16 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   http://localhost:8082/filteredimage?image_url=https%3A%2F%2Fcdn.motor1.com%2Fimages%2Fmgl%2Fmrz1e%2Fs1%2Fcoolest-cars-feature.jpg&imgrefurl=https%3A%2F%2Fwww.motor1.com%2Ffeatures%2F447037%2Fcoolest-cars%2F&tbnid=z4t7U0uZlwRpRM&vet=12ahUKEwi73Nv5yuX5AhXvSkEAHbdtBJoQMygNegUIARCSAw..i&docid=LBt-IiFPpVXXIM&w=1920&h=1080&q=car&ved=2ahUKEwi73Nv5yuX5AhXvSkEAHbdtBJoQMygNegUIARCSAw */
 
   app.get("/filteredimage", async (req: express.Request, res: express.Response) => {
-      let { image_url } = req.query;
+  
+  let { image_url } = req.query;
+    
 
 //   1. validate the image_url query
       if (!image_url) {
         res.status(400).send("Image url required!!!");
       } else {
 //   2. call filterImageFromURL(image_url) to filter the image
-        await filterImageFromURL(image_url)
+        await filterImageFromURL(image_url.toString())
           .then(function (image_filtered_path) {
 //   3. send the resulting file in the response
             res.sendFile(image_filtered_path, () => {
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import { ParsedUrlQuery } from "querystring";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
